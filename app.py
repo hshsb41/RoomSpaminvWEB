f3009ae805982ac340f2e42c3504d269c45d562b
hshsb41 <nansnanans0@gmail.com>
2026-04-10 22:39:09 +0545

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+import requests
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send_spam', methods=['POST'])
+def send_spam():
+    data = request.get_json()
+    user_id = data.get('user_id')
+    
+    if not user_id:
+        return jsonify({"status": "error", "message": "UID is required!"}), 400
+
+    # API Endpoint
+    api_url = "https://attacks-boulevard-relevant-praise.trycloudflare.com/spam"
+    params = {'user_id': user_id, 'duration': 5}
+
+    try:
+        response = requests.get(api_url, params=params, timeout=15)
+        return jsonify({"status": "success", "message": "Command Sent!"})
+    except:
+        return jsonify({"status": "error", "message": "API Connection Failed"}), 500
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
